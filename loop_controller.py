37c431a7c952c104854af1eba53337b414f5ade7
satish <satjhariya@gmail.com>
2025-09-21 00:55:48 +0530

    result attirbute added to take out the combined result

--- loop_controller.py
+++ loop_controller.py
@@ -9,39 +9,44 @@ class loop_cntrl:
         self.i = None
         self.j = None
         self.k = None
-    
+
+        self.result = {}
+
     def loop4(self):
         if self.it4 is None:
             print("loop 4 is skipped no iterator 4")
             return
         for l in self.it4:
             print(f"Loop 4 i = {self.i}, j = {self.j}, k = {self.k}, l = {l}")
-
+            self.result[self.i][self.j][self.k][l] = l
+            
+            
     def loop3(self):
         if self.it3 is None:
             print("loop 3 is skipped no iterator 3")
             return
         for k in self.it3:
+            self.result[self.i][self.j][k] = {}
             self.k = k
             print(f"Loop 3 i = {self.i}, j = {self.j}, k = {self.k}")
             self.loop4()
-
 
     def loop2(self):
         if self.it2 is None:
             print("loop 2 is skipped no iterator 2")
             return
         for j in self.it2:
+            self.result[self.i][j] ={}
             self.j = j
             print(f"Loop 2 i = {self.i}, j = {self.j}")
             self.loop3()
-
 
     def loop1(self):
         if self.it1 is None:
             print("loop 1 is skipped no iterator 1")
             return
         for i in self.it1:
+            self.result[i] = {}
             self.i = i
             print(f"Loop 1 i = {self.i}")
             self.loop2()
@@ -61,14 +66,5 @@ if __name__ == "__main__":
 
     p1 = loop_cntrl(range(1),range(2),range(3),range(4))
     p1.run()
+    print(p1.result)
     print(f"\n\n")
-    p2 = loop_cntrl(None,None,range(3),range(4))
-    p2.run()
-    print(f"\n\n")
-
-    p3 = loop_cntrl(None,None,None,range(4))
-    p3.run()
-    print(f"\n\n")
-
-    p4 = loop_cntrl(None,range(2),None,range(4))
-    p4.run()
